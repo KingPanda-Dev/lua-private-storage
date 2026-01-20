c61a3d525de05c38eff5e3dadbc87b55a1b33511
KingPanda-Dev <87371660+KingPanda-Dev@users.noreply.github.com>
2026-01-20 21:34:17 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+export const FILE_CODES = {
+  "eco.lua": "ECO-SECRET-2026",
+  "farm.lua": "FARM-LOCK-777",
+  "admin.lua": "ADMIN-ROOT-X"
+}
+
+export const GITHUB = {
+  OWNER: "KingPanda-Dev",
+  REPO: "lua-private-storage",
+  BRANCH: "main",
+  TOKEN: process.env.GH_TOKEN
+}
