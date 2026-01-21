c34a49fcac2ba57006547f6c4db166e5f8f866a8
KingPanda-Dev <87371660+KingPanda-Dev@users.noreply.github.com>
2026-01-22 02:47:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,34 @@
-export const FILE_CODES = {
-  "eco.lua": "ECO-SECRET-2026",
-  "farm.lua": "FARM-LOCK-777",
-  "admin.lua": "ADMIN-ROOT-X"
+export const DISCORD = {
+  CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+  CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+  REDIRECT_URI: process.env.DISCORD_REDIRECT_URI
+}
+
+export const PERMISSION = {
+  OWNERS: [
+    "ISI_DISCORD_ID_KAMU"
+  ],
+  ALLOWED_USERS: [
+    "ISI_DISCORD_ID_KAMU"
+  ]
+}
+
+export const SCRIPT_CATEGORIES = {
+  growpai: {
+    name: "GrowPai Lua",
+    files: ["growpai.lua"]
+  },
+  growlauncher: {
+    name: "GrowLauncher Lua",
+    files: ["growlauncher.lua"]
+  }
 }
 
 export const GITHUB = {
-  OWNER: "KingPanda-Dev",
+  OWNER: "USERNAME_GITHUB",
   REPO: "lua-private-storage",
   BRANCH: "main",
   TOKEN: process.env.GH_TOKEN
 }
+
+export const WEBHOOK_URL = ""
