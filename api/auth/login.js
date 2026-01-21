ea2175e40951ae5c654e62ea7cc0fb4f03e8c84f
KingPanda-Dev <87371660+KingPanda-Dev@users.noreply.github.com>
2026-01-22 03:02:01 +0700

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,1 +1,13 @@
+import { DISCORD } from "../../config.js"
 
+export default function handler(req, res) {
+  const url =
+    `https://discord.com/oauth2/authorize` +
+    `?client_id=${DISCORD.CLIENT_ID}` +
+    `&redirect_uri=${encodeURIComponent(DISCORD.REDIRECT_URI)}` +
+    `&response_type=code` +
+    `&scope=identify email`
+
+  res.writeHead(302, { Location: url })
+  res.end()
+}
