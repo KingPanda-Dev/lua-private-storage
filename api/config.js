c82b1bcb12c329c70b9129e1cba489a2f532a543
KingPanda-Dev <87371660+KingPanda-Dev@users.noreply.github.com>
2026-01-26 03:17:00 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,1 +1,9 @@
-
+export default function handler(req, res) {
+  // Jangan bocorin semua env ya, hanya yang aman aja
+  res.json({
+    ownerIds: (process.env.OWNER_DISCORD_IDS || "")
+      .split(",")
+      .map(x => x.trim())
+      .filter(Boolean)
+  })
+}
